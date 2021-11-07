d10a813649fb930e8b7a85c9b4ab032905ae8204
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2021-11-07 09:03:09 -0800

    ensure strings are null-terminated; thanks CRYPTO TICK

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ the included Makefile should help with that.
 Then, create a file called `vminer.conf` and paste 
 
 ```
-USERNAME = "hugh"
 PREVIOUS_BLOCK_HASH = "4FD097A91E71DF403E6159FC58C53ECE1A809708549073541912BCBD10C924EA"
 MESSAGE = "IS GENESIS BLOCK VENONA. ARE WISHING FOR ALL GOOD LUCK."
 THRESHOLD = "0000020000000000000000000000000000000000000000000000000000000000"
@@ -18,5 +17,5 @@ THRESHOLD = "0000020000000000000000000000000000000000000000000000000000000000"
 
 Obviously, you will need to modify those variables to the appropriate values.
 
-Finally, `./bin/vminer vminer.conf` or `.\bin\vminer.exe vminer.conf` (on
-Windows) and wait!
+Finally, `./bin/vminer "your username" vminer.conf` (or
+`.\bin\vminer.exe "your username" vminer.conf` on Windows) and wait!
--- src/main.c
+++ src/main.c
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
     assert(16 == strlen(MAGIC_BYTES ));
     assert( 4 == strlen(AUTHOR_TOKEN));
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("usage: %s <vminer.conf>\n", argv[0]);
+        printf("usage: %s <username> <vminer.conf>\n", argv[0]);
         return 1;
     }
 
@@ -40,7 +40,11 @@ int main(int argc, char *argv[])
     args.message                = NULL;
     args.threshold              = NULL;
 
-    FILE *fh = fopen(argv[1], "r");
+    args.username = calloc(17, sizeof(char));
+    memset(args.username, ' ', 16);
+    memcpy(args.username, argv[1], strlen(argv[1])); 
+
+    FILE *fh = fopen(argv[2], "r");
     char buf1[256], buf2[256];
     unsigned int ln = 0;
     unsigned int errors = 0;
@@ -53,33 +57,6 @@ int main(int argc, char *argv[])
         // octothorpes,) we don't need them.
         if (sscanf(buf1, " %s", buf2) == EOF) continue;
         if (sscanf(buf1, " %[#]", buf2) == 1) continue;
-
-        // Get USERNAME. 
-        if (
-            sscanf(buf1, " USERNAME = \"%[^\"]\"", buf2) == 1
-            || sscanf(buf1, " USERNAME = '%[^']'", buf2) == 1
-            || sscanf(buf1, " USERNAME = %s", buf2) == 1)
-        {
-            if (args.username != NULL)
-            {
-                fprintf(stderr,
-                    "ERROR: Overwriting USERNAME on line %d.\n", ln);
-                ++errors;
-            } 
-
-            if (strlen(buf2) > 16)
-            {
-                fprintf(stderr,
-                    "ERROR: The USERNAME specified on line %d, \"%s\", is too long.", ln, buf2);
-                ++errors;
-            }
-
-            args.username = calloc(16, sizeof(char));
-            memset(args.username, ' ', 16);
-            memcpy(args.username, buf2, strlen(buf2)); 
-
-            continue;
-        }
 
         // Get PREVIOUS_BLOCK_HASH.
         if (
@@ -101,7 +78,7 @@ int main(int argc, char *argv[])
                 ++errors;
             }
 
-            args.previous_block_hash = calloc(64, sizeof(char));
+            args.previous_block_hash = calloc(65, sizeof(char));
             memcpy(args.previous_block_hash, buf2, 64);
         
             continue;
@@ -126,7 +103,7 @@ int main(int argc, char *argv[])
                 ++errors;
             }
 
-            args.message = calloc(64, sizeof(char));
+            args.message = calloc(65, sizeof(char));
             memset(args.message, ' ', 64);
             memcpy(args.message, buf2, strlen(buf2)); 
 
@@ -155,7 +132,7 @@ int main(int argc, char *argv[])
 
             // Convert the hex string to a uint8_t array.
             char *it = buf2;
-            args.threshold = calloc(32, sizeof(uint8_t));
+            args.threshold = calloc(33, sizeof(uint8_t));
             for (unsigned int i = 0; i < 32; ++i, it = it + 2)
                 sscanf(it, "%2hhx", &args.threshold[i]);
             
@@ -168,7 +145,7 @@ int main(int argc, char *argv[])
     }
 
     // Check for unspecified arguments.
-    if (args.username            == NULL) { fprintf(stderr, "ERROR: USERNAME is undefined; set it in %s.\n"           , argv[1]); ++errors; }
+    if (args.username            == NULL) { fprintf(stderr, "ERROR: USERNAME is undefined; set it on the command line\n"); }
     if (args.previous_block_hash == NULL) { fprintf(stderr, "ERROR: PREVIOUS_BLOCK_HASH is undefined; set it in %s.\n", argv[1]); ++errors; }
     if (args.message             == NULL) { fprintf(stderr, "ERROR: MESSAGE is undefined; set it in %s.\n"            , argv[1]); ++errors; }
     if (args.threshold           == NULL) { fprintf(stderr, "ERROR: THRESHOLD is undefined; set it in %s.\n"          , argv[1]); ++errors; }
